4b67598f244cfc155cc6279f847c47ea5fba59c8
averak <abe12@mccc.jp>
2021-12-05 00:01:14 +0900

    [feat] create spring-boot application entrypoint

---
+++ app/src/main/java/dev/abelab/gifttree/GiftTreeApplication.java
@@ -0,0 +1,13 @@
+package dev.abelab.gifttree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftTreeApplication.class, args);
+    }
+
+}
