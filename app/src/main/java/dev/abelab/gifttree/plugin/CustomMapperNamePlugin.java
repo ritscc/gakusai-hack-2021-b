527eb072e122d4ba3623b7db3c6c999cb93c897c
averak <abe12@mccc.jp>
2021-12-05 00:37:02 +0900

    [feat] generate entity & mapper by mybatis

---
+++ app/src/main/java/dev/abelab/gifttree/plugin/CustomMapperNamePlugin.java
@@ -0,0 +1,32 @@
+package dev.abelab.gifttree.plugin;
+
+import java.util.List;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+/**
+ * Custom MyBatis Mapper Name
+ */
+public class CustomMapperNamePlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(final List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public void initialized(final IntrospectedTable introspectedTable) {
+        super.initialized(introspectedTable);
+
+        // NOTE: XxxMapper.javaは手動で作成してください
+        // XxxMapper.java -> XxxBaseMapper.java
+        final var javaMapperName = introspectedTable.getMyBatis3JavaMapperType();
+        introspectedTable.setMyBatis3JavaMapperType(javaMapperName.replaceAll("Mapper$", "BaseMapper"));
+
+        // XxxMapper.xml -> XxxBaseMapper.xml
+        final var xmlMapperName = introspectedTable.getMyBatis3XmlMapperFileName();
+        introspectedTable.setMyBatis3XmlMapperFileName(xmlMapperName.replaceAll("Mapper\\.xml", "BaseMapper.xml"));
+    }
+
+}
