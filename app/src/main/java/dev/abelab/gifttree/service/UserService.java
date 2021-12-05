c0ac20cf447a6d6e3f1626b4a651b7634fe018c5
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-12-06 01:44:12 +0900

    [feat] create get users

--- app/src/main/java/dev/abelab/gifttree/service/UserService.java
+++ app/src/main/java/dev/abelab/gifttree/service/UserService.java
@@ -1,5 +1,6 @@
 package dev.abelab.gifttree.service;
 
+import dev.abelab.gifttree.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.modelmapper.ModelMapper;
@@ -8,11 +9,28 @@ import lombok.*;
 import dev.abelab.gifttree.api.response.UserResponse;
 import dev.abelab.gifttree.db.entity.User;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
     private final ModelMapper modelMapper;
+
+    /**
+     * ユーザリストを取得
+     *
+     * @param loginUser ログインユーザ
+     *
+     * @return ユーザリスト
+     */
+    @Transactional
+    public List<User> getUsers(final User loginUser) {
+        // ユーザリストの取得
+        return this.userRepository.selectAll();
+    }
 
     /**
      * ログインユーザのプロフィールを取得
