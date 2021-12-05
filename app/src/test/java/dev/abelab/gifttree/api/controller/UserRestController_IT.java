3ca07230133d7fd6c9ac9cca598807fa44629c92
averak <abe12@mccc.jp>
2021-12-06 02:42:10 +0900

    [test] get users api integration test

--- app/src/test/java/dev/abelab/gifttree/api/controller/UserRestController_IT.java
+++ app/src/test/java/dev/abelab/gifttree/api/controller/UserRestController_IT.java
@@ -1,5 +1,8 @@
 package dev.abelab.gifttree.api.controller;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.*;
 
@@ -12,7 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.modelmapper.ModelMapper;
 
 import dev.abelab.gifttree.api.response.UserResponse;
+import dev.abelab.gifttree.api.response.UsersResponse;
 import dev.abelab.gifttree.db.mapper.UserMapper;
+import dev.abelab.gifttree.helper.sample.UserSample;
+import dev.abelab.gifttree.helper.util.RandomUtil;
 import dev.abelab.gifttree.exception.ErrorCode;
 import dev.abelab.gifttree.exception.NotFoundException;
 import dev.abelab.gifttree.exception.UnauthorizedException;
@@ -24,6 +30,7 @@ public class UserRestController_IT extends AbstractRestController_IT {
 
 	// API PATH
 	static final String BASE_PATH = "/api/users";
+	static final String GET_USERS_PATH = BASE_PATH;
 	static final String GET_LOGIN_USER_PATH = BASE_PATH + "/me";
 
 	@Autowired
@@ -85,6 +92,58 @@ public class UserRestController_IT extends AbstractRestController_IT {
 			 * test & verify
 			 */
 			final var request = getRequest(GET_LOGIN_USER_PATH);
+			request.header(HttpHeaders.AUTHORIZATION, "");
+			execute(request, new UnauthorizedException(ErrorCode.INVALID_ACCESS_TOKEN));
+		}
+
+	}
+
+	/**
+	 * ユーザ一覧取得APIの統合テスト
+	 */
+	@Nested
+	@TestInstance(PER_CLASS)
+	class GetUsers_IT extends AbstractRestControllerInitialization_IT {
+
+		@Test
+		void 正_ユーザ一覧を取得() throws Exception {
+			/*
+			 * given
+			 */
+			final var loginUser = createLoginUser(true);
+			final var credentials = getLoginUserCredentials(loginUser);
+
+			final var users = Arrays.asList( //
+				loginUser, //
+				UserSample.builder().email(RandomUtil.generateEmail()).build(), //
+				UserSample.builder().email(RandomUtil.generateEmail()).build() //
+			);
+			userMapper.insert(users.get(1));
+			userMapper.insert(users.get(2));
+
+			/*
+			 * test
+			 */
+			final var request = getRequest(GET_USERS_PATH);
+			request.header(HttpHeaders.AUTHORIZATION, credentials);
+			final var response = execute(request, HttpStatus.OK, UsersResponse.class);
+
+			/*
+			 * verify
+			 */
+			assertThat(response.getUsers()) //
+				.extracting(UserResponse::getId, UserResponse::getEmail, UserResponse::getName) //
+				.containsExactlyElementsOf(
+					users.stream().map(user -> tuple(user.getId(), user.getEmail(), user.getName())).collect(Collectors.toList()));
+			response.getUsers().forEach(user -> assertThat(user.getIconUrl()).isNotNull());
+		}
+
+		@Test
+		void 異_無効な認証ヘッダ() throws Exception {
+			/*
+			 * test & verify
+			 */
+			final var request = getRequest(GET_USERS_PATH);
 			request.header(HttpHeaders.AUTHORIZATION, "");
 			execute(request, new UnauthorizedException(ErrorCode.INVALID_ACCESS_TOKEN));
 		}
