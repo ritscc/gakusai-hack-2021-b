04668b6bd6baf38e5edff25723c7396d66e6c619
averak <abe12@mccc.jp>
2021-12-06 02:16:20 +0900

    [feat] create home page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// components
+import { HomeComponent } from './component/page/home/home.component';
+import { HeaderComponent } from './component/page/header/header.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'ranking',
+    component: HeaderComponent,
+  },
+  {
+    path: 'login',
+    component: HeaderComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
