dfbe935f9c13bd5ff874083cf0ac632d841e39c9
averak <abe12@mccc.jp>
2021-12-06 16:26:41 +0900

    [feat] guard if not logged in

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// guards
+import { AuthGuard } from 'src/app/shared/guard/auth.guard';
+
 // components
 import { HomeComponent } from './component/page/home/home.component';
+import { SignupComponent } from './component/page/signup/signup.component';
 import { HeaderComponent } from './component/page/header/header.component';
 import { RankingComponent } from './component/page/ranking/ranking.component';
 import { ProfileComponent } from './component/page/profile/profile.component';
@@ -13,8 +17,13 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
     path: '',
     component: HeaderComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'ranking',
