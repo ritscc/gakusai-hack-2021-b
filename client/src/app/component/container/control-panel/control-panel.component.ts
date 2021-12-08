64f69ee1c94562f91b2cac6ef95d4b570d4409ab
averak <abe12@mccc.jp>
2021-12-09 05:13:01 +0900

    [feat] qr code loader

--- client/src/app/component/container/control-panel/control-panel.component.ts
+++ client/src/app/component/container/control-panel/control-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { QrLoaderDialogComponent } from 'src/app/component/container/qr-loader-dialog/qr-loader-dialog.component';
 
 @Component({
   selector: 'app-control-panel',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./control-panel.component.css'],
 })
 export class ControlPanelComponent implements OnInit {
-  constructor() {}
+  constructor(private matDialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  launchCamera(): void {
+    this.matDialog.open(QrLoaderDialogComponent);
+  }
 }
